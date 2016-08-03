1b6c2f136f2f8b1e7149b48fd7a9a96a6ae6245f
buruoxixue <1571861967@qq.com>
2016-08-03 18:09:13 +0800

    提交啦啦

--- app/src/main/java/com/example/administrator/chushoutv/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/chushoutv/activity/MainActivity.java
@@ -6,13 +6,10 @@ import android.os.Bundle;
 import com.example.administrator.chushoutv.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
 
     }
