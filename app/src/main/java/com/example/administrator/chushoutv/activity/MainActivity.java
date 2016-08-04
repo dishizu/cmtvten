d18ee3087338d983717ca9940db98b2c3312b266
buruoxixue <1571861967@qq.com>
2016-08-04 11:06:32 +0800

    添加了tablala

--- app/src/main/java/com/example/administrator/chushoutv/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/chushoutv/activity/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.administrator.chushoutv.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.administrator.chushoutv.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tab_viewPager)
+    ViewPager tab_viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
